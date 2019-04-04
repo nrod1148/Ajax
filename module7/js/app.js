e5988c629c71b46230406cb43b0e97a4f4d24c96
Nicolas Rodriguez <nrodriguez1148@gmail.com>
2019-04-04 10:18:58 -0400

    Assignment 7 Solution

--- module7/js/app.js
+++ module7/js/app.js
@@ -15,20 +15,10 @@ ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyController(ShoppingListCheckOffService) {
   var toBuy = this;
 
-  // Use factory to create new shopping list service
-  // var shoppingList = ShoppingListCheckOffService();
-
   toBuy.toBuyItems = ShoppingListCheckOffService.getToBuyItems();
 
-  // list1.itemName = "";
-  // list1.itemQuantity = "";
-
-  // list1.addItem = function () {
-  //   shoppingList.addItem(list1.itemName, list1.itemQuantity);
-  // }
-
-  toBuy.buyItem = function (itemIndex) {
-    ShoppingListCheckOffService.buyItem(itemIndex);
+  toBuy.buyItem = function (itemIndex, itemQuantity) {
+  ShoppingListCheckOffService.buyItem(itemIndex, itemQuantity);
   };
 }
 
@@ -38,55 +28,21 @@ function AlreadyBoughtController(ShoppingListCheckOffService) {
   var bought = this;
 
   bought.boughtItems = ShoppingListCheckOffService.getBoughtItems();
-
-  // // Use factory to create new shopping list service
-  // var shoppingList = ShoppingListFactory();
-
-  // list2.items = shoppingList.getItems();
-
-  // list2.itemName = "";
-  // list2.itemQuantity = "";
-
-  // list2.addItem = function () {
-  //   try {
-  //     shoppingList.addItem(list2.itemName, list2.itemQuantity);
-  //   } catch (error) {
-  //     list2.errorMessage = error.message;
-  //   }
-
-  // }
-
-  // list2.removeItem = function (itemIndex) {
-  //   shoppingList.removeItem(itemIndex);
-  // };
 }
-
 
 function ShoppingListCheckOffService() {
   var service = this;
 
   // List of shopping items
+  var toBuyItems = [{name: "egg(s)", quantity: 12, pricePerItem: .20}, {name: "apple(s)", quantity: 6, pricePerItem: .55}, 
+    {name: "juice(s)", quantity: 3, pricePerItem: .79}, {name: "onion(s)", quantity: 5, pricePerItem: .70}, 
+    {name: "steaks(s)", quantity: 2, pricePerItem: 7.11}];
 
-  var toBuyItems = [{name: "eggs", quantity: 12, pricePerItem: 20}, {name: "milk cartons", quantity: 2, pricePerItem: 150}, 
-    {name: "tomatoes", quantity: 4, pricePerItem: 50}, {name: "onions", quantity: 2, pricePerItem: 70}, 
-    {name: "strawberries", quantity: 22, pricePerItem: 11}];
+  service.itemQuantity = "";
 
-  // service.addItem = function (itemName, quantity) {
-  //   if ((maxItems === undefined) ||
-  //       (maxItems !== undefined) && (items.length < maxItems)) {
-  //     var item = {
-  //       name: itemName,
-  //       quantity: quantity
-  //     };
-  //     items.push(item);
-  //   }
-  //   else {
-  //     throw new Error("Max items (" + maxItems + ") reached.");
-  //   }
-  // };
+  service.buyItem = function (itemIndex, quantity) {
 
-  service.buyItem = function (itemIndex) {
-    service.addItem(toBuyItems[itemIndex].name, toBuyItems[itemIndex].quantity, toBuyItems[itemIndex].pricePerItem);
+    service.addItem(toBuyItems[itemIndex].name, quantity, toBuyItems[itemIndex].pricePerItem);
     toBuyItems.splice(itemIndex, 1);
     
   };
@@ -98,12 +54,15 @@ function ShoppingListCheckOffService() {
   var boughtItems = [];
 
   service.addItem = function (itemName, itemQuantity, pricePerItem) {
+
+    var totalPrice = itemQuantity * pricePerItem;
+
     var boughtItem = {
       name: itemName,
       quantity: itemQuantity,
-      pricePerItem:  pricePerItem
+      totalPrice:  totalPrice
     };
-      
+
     boughtItems.push(boughtItem);
 
   };  
