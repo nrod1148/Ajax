d428e479f6f334ccf56bb1cb07c3dc10052568cd
Nicolas Rodriguez <nrodriguez1148@gmail.com>
2019-04-30 21:51:44 -0400

    Module 9 Solution

---
+++ module9/src/categories.component.js
@@ -0,0 +1,13 @@
+(function() {
+	
+'use strict';
+
+angular.module('MenuApp')
+  .component('categories', {
+    templateUrl: 'src/categoriescomponent.template.html',
+    bindings: {
+      items: '<'
+    }
+  });
+
+})();
---
+++ module9/src/items.component.js
@@ -0,0 +1,13 @@
+(function() {
+
+'use strict';
+
+angular.module('MenuApp')
+  .component('items', {
+    templateUrl: 'src/itemscomponent.template.html',
+    bindings: {
+      items: '<'
+    }
+  });
+
+})();
